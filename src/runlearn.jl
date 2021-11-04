f9ae6249505f398452cdf884fa3ae4379876cbb4
rafabench <rafa.bench@gmail.com>
2021-11-04 10:40:13 -0300

    add callback function and update notebooks

--- src/runlearn.jl
+++ src/runlearn.jl
@@ -1,4 +1,25 @@
-function train_ode_dnn(dataset, nlayers, channels, rkmethod, niter, τ, τ_max, stepsize, problem)
+mutable struct Logger
+    loss::Array{Float64,1}
+    grad_norm::Array{Float64,1}
+    accuracy::Array{Float64,1}
+    time::Array{Float64,1}
+    function Logger()
+        loss = []
+        grad_norm = []
+        accuracy = []
+        time = []
+        new(loss,grad_norm,accuracy,time)
+    end
+end
+
+function calculate_accuracy(Ctrls,rkmethod,Problem)
+    S = RKforwardstepper(Ctrls,rkmethod,Problem)
+    Classifier = S.Classifier
+    acc = sum(round.(Classifier) .== Problem.C)/length(Problem.C)
+    return acc
+end
+
+function train_ode_dnn(dataset, nlayers, channels, rkmethod, niter, τ, τ_max, stepsize, problem, cb)
     F = zeros(niter)
     Fn = zeros(niter)
 
@@ -7,18 +28,19 @@ function train_ode_dnn(dataset, nlayers, channels, rkmethod, niter, τ, τ_max, 
 
     Ctrls = ControlDef(dataset, stepsize, channels, nlayers);
     Y0, C = dataset
+    log = Logger()
     for iter in 1:niter
+        time1 = time_ns()
         Gradient=GradientCalc(Ctrls, rkmethod, problem);
         Ctrls,α,normGsq = backtracking(Ctrls,Gradient,rkmethod,problem,minimum([τ_max,2*α]));
-        F[iter] = objective(Ctrls, rkmethod, problem);
-        Fn[iter] = sqrt(normGsq);
-        if rem(iter,2000) == 0 || iter == 1
-            @printf("\nMethod=%s, nlayers=%d\n",rkmethod.name,nlayers)
-            @printf("Iteration no: %d\n",iter)
-            @printf("Value of alpha: %7.4f\n",α);
-            @printf("Residual: %8.4f\n",F[iter])
-            @printf("Norm Grad: %8.4e\n",Fn[iter]);
-        end
+        time2 = time_ns()
+        Δt = (time2 - time1)/1e9
+        push!(log.loss,objective(Ctrls, rkmethod, problem))
+        push!(log.grad_norm,sqrt(normGsq))
+        push!(log.accuracy,calculate_accuracy(Ctrls,rkmethod,problem))
+        push!(log.time,Δt)
+
+        cb(iter, rkmethod, nlayers, α, log, Ctrls);
     end
-    return Ctrls,F,Fn
+    return Ctrls,log
 end
